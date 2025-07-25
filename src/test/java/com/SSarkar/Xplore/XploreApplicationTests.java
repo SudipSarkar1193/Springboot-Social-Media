dfaf8a403375ed7b5e004d0d63ee568bef563c7b
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2025-07-26 02:03:08 +0530

    started working on Social media project 'Xplore'

---
+++ src/test/java/com/SSarkar/Xplore/XploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SSarkar.Xplore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
