619216d6e6baa73a52c6c3f961412b2f9bd536da
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2025-07-28 09:45:33 +0530

    Added DTOs for login request & response

---
+++ src/main/java/com/SSarkar/Xplore/dto/LoginRequestDTO.java
@@ -0,0 +1,14 @@
+package com.SSarkar.Xplore.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequestDTO {
+
+    @NotBlank(message = "Username cannot be blank")
+    private String username;
+
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+}
