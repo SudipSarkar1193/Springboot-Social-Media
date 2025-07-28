5b82cd3108ec2c42b88196479b616962a86fda22
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2025-07-28 23:22:43 +0530

    Added Comment Request DTO

---
+++ src/main/java/com/SSarkar/Xplore/dto/CommentRequestDTO.java
@@ -0,0 +1,18 @@
+package com.SSarkar.Xplore.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CommentRequestDTO {
+
+    @NotBlank(message = "Comment content cannot be blank")
+    @Size(min = 1, max = 320, message = "Content must be between 1 and 320 characters")
+    private String content;
+
+    // The field you requested
+    private List<String> imageUrls;
+}
