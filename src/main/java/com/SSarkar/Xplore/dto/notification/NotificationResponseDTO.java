cd25519034938b0b5139f9795a67a7ebd1ce924f
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2025-07-29 20:02:46 +0530

    Added NotificationResponseTDO

---
+++ src/main/java/com/SSarkar/Xplore/dto/notification/NotificationResponseDTO.java
@@ -0,0 +1,23 @@
+package com.SSarkar.Xplore.dto.notification;
+
+import com.SSarkar.Xplore.entity.enums.NotificationType;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class NotificationResponseDTO {
+    private UUID notificationUuid;
+    private NotificationType type;
+    private UUID senderUuid;
+    private String senderUsername;
+    private String senderProfilePictureUrl;
+    private UUID relatedEntityUuid; // e.g., Post UUID for likes/comments, User UUID for follows
+    private String relatedEntityTitle; // e.g., Post title for likes/comments, User username
+    private boolean isRead;
+    private Instant createdAt;
+    private String message; // A human-readable message generated on the backend
+}
