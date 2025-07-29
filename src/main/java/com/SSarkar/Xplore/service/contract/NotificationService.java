d44dc5a4c2e2955c34c3f3f645a84846f08235ba
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2025-07-29 20:08:48 +0530

    Added Notification Service interface (contract)

---
+++ src/main/java/com/SSarkar/Xplore/service/contract/NotificationService.java
@@ -0,0 +1,17 @@
+package com.SSarkar.Xplore.service.contract;
+
+import com.SSarkar.Xplore.dto.notification.NotificationResponseDTO;
+import com.SSarkar.Xplore.dto.post.PagedResponseDTO;
+import com.SSarkar.Xplore.entity.User;
+import com.SSarkar.Xplore.entity.enums.NotificationType;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface NotificationService {
+
+    void createNotification(User sender, User recipient, NotificationType type, UUID relatedEntityUuid);
+
+    PagedResponseDTO<NotificationResponseDTO> getNotificationsForUser(UserDetails currentUserDetails, Pageable pageable);
+}
