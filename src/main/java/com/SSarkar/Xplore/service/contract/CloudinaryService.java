de3ca3acde5452577397d1514d9efd2326282d4e
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2025-09-02 21:26:58 +0530

    Started working on shots features - modified CloudinaryService

--- src/main/java/com/SSarkar/Xplore/service/contract/CloudinaryService.java
+++ src/main/java/com/SSarkar/Xplore/service/contract/CloudinaryService.java
@@ -5,5 +5,6 @@ import java.io.IOException;
 public interface CloudinaryService {
     String upload(String base64Image) throws IOException;
     String upload(byte[] data) throws IOException; // Overloaded method to upload raw byte data
+    String uploadVideo(byte[] data) throws IOException;
     void delete(String imageUrl) throws IOException;
 }
